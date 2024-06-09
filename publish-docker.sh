e856db5fe26a6255f85b575e80f5204650a64c1a
Isabela_ <isabela.rosero@correounivalle.edu.co>
2024-06-09 13:57:20 -0500

    orchestrator routes

--- publish-docker.sh
+++ publish-docker.sh
@@ -1,13 +1,13 @@
 docker login
 
-docker build -t ms_user_server ms-user/
+docker build -t ms_reviews_server ms-reviews/
 
-docker tag ms_user_server cristiank/ms_user_server
+docker tag ms_reviews_server isabela22/ms_reviews_server
 
-docker tag push cristiank/ms_user_server
+docker tag push isabela22/ms_reviews_server
 
-docker build -t ms_user_db ms-user/db/
+docker build -t ms_reviews_db ms-reviews/db/
 
-docker tag  ms_user_db cristiank/ms_user_db
+docker tag  ms_reviews_db isabela22/ms_reviews_db
 
-docker tag push cristiank/ms_user_db
+docker tag push isabela22/ms_reviews_db
